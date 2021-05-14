35bbcd7cb44c38fa6e4247e38b939191b8073905
BLOT Martial <martial.blot@outlook.fr>
2021-05-14 19:24:51 +0200

    Added animation to css folder

--- src/cssfiles/master.js
+++ src/cssfiles/master.js
@@ -1,6 +1,13 @@
 export const master = [
-  { button_1: { name: 'MartyButton', category: 'button' } },
-  { button_2: { name: 'NicoButton', category: 'button' } },
+  {
+    animation_1: {
+      folder: 'animation_1',
+      name: 'NicoButton',
+      category: 'button',
+    },
+  },
+  { button_1: { folder: 'button_1', name: 'MartyButton', category: 'button' } },
+  { button_2: { folder: 'button_2', name: 'NicoButton', category: 'button' } },
 ]
 
 export default master
