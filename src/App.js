35953057764dcae5ec568c2fa0e051104235cff0
nicolasvienot <nvienot@gmail.com>
2021-05-14 22:21:43 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Router } from '@reach/router'
 import { Home } from './pages/Home'
+import { Project } from './pages/Project'
 
 const App = () => (
-  <>
-    <Home />
-  </>
+  <Router>
+    <Home path="/" />
+    <Project path="/project/:id" />
+  </Router>
 )
 
 export default App
